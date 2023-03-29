5eace9734cd8f81b003557208088dfde60bebc3d
EriclesCalvt <ericlesprogrammer@gmail.com>
2023-03-28 21:53:44 -0300

    Open Source Code

---
+++ crudSERV/script_SERV.js
@@ -0,0 +1,19 @@
+const url$ = document.getElementById("btn_Consult");
+const urlRote$ = document.getElementById("urlRote");
+const urlRote3$ = document.getElementById("button_especialidade");
+
+url$.addEventListener("click", () => {
+  window.location.href = "../../loginPage/Login.html";
+});
+
+// urlRote3$.addEventListener("click", () => {
+//   window.location.href = //ADICIONAR ROTA;
+// });
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // adicionar o envio dos dados para o localStorage
+
+  window.location.href = "../especialidades_User/especialidades.html";
+});
