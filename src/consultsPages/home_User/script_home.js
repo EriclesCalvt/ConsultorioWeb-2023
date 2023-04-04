dbd213a1562dad15d94415dc914756c83f4f427c
onlywillian <willianandrade953@gmail.com>
2023-04-04 08:26:15 -0300

    source-code

--- src/consultsPages/home_User/script_home.js
+++ src/consultsPages/home_User/script_home.js
@@ -1,9 +1,4 @@
-const url$ = document.getElementById("btn_Consult");
 const urlRote$ = document.getElementById("urlRote");
-
-url$.addEventListener("click", () => {
-  window.location.href = "../../loginPage/Login.html";
-});
 
 urlRote$.addEventListener("click", () => {
   window.location.href = "../dados_User/dados_User.html";
