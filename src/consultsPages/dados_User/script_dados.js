cb8a31b75a6164f28752fc7c083a1806bc5acb1c
EriclesCalvt <ericlesprogrammer@gmail.com>
2023-04-17 14:42:46 -0300

    Front-end

--- src/consultsPages/dados_User/script_dados.js
+++ src/consultsPages/dados_User/script_dados.js
@@ -5,10 +5,43 @@ url$.addEventListener("click", () => {
   window.location.href = "../../loginPage/Login.html";
 });
 
-document.querySelector('form').addEventListener('submit', e => {
-  e.preventDefault()
-
+document.querySelector('.form-user').addEventListener('submit', (e) => {  
   // adicionar o envio dos dados para o localStorage
+  e.preventDefault()
+  const NameInput = document.getElementById("Nome");
+  GetName = localStorage.getItem("Nome");
+  localStorage.setItem("Nome", NameInput.value);
+  NameInput.value = ""
 
-  window.location.href = "../especialidades_User/especialidades.html";
+  const RgInput = document.getElementById("RG");
+  GetRg = localStorage.getItem("RG");
+  localStorage.setItem("RG", RgInput.value);
+  RgInput.value = ""
+  
+  const CpfInput = document.getElementById("CPF");
+  GetCpf = localStorage.getItem("CPF");
+  localStorage.setItem("CPF", CpfInput.value);
+  CpfInput.value = ""
+
+  const DataInput = document.getElementById("Data");
+  localStorage.setItem("Data-Nascimento", DataInput.value);
+  GetData = localStorage.getItem("Data-Nascimento");
+  console.log(GetData)
+  DataInput.value = ""
+
+  //Condicional:
+  const items = {
+    NameInput : GetName,
+    RgInput : GetRg,
+    CpfInput : GetCpf,
+    DataInput : GetData,
+  };
+
+  if(items == false){
+  }else{
+    window.location.href = "../especialidades_User/especialidades.html";
+  }
+
+
+
 })
