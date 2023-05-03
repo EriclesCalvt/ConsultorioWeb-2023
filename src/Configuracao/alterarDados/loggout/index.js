f5bcaf53550b9a67d8dd33b6b541f6c4ff180d90
EriclesCalvt <ericlesprogrammer@gmail.com>
2023-05-03 11:23:32 -0300

    update

---
+++ src/Configuracao/alterarDados/loggout/index.js
@@ -0,0 +1,13 @@
+const url$ = document.getElementById("btn_Consult");
+const urlRote$ = document.getElementById("urlRote");
+url$.addEventListener("click", () => {
+  window.location.href = "../../loginPage/Login.html";
+});
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // adicionar o envio dos dados para o localStorage
+
+  window.location.href = "../especialidades_User/especialidades.html";
+});
