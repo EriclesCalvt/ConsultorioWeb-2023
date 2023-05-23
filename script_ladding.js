a3f8d413d82260580f5bdfc5ac089e3d91751659
EriclesCalvt <ericlesprogrammer@gmail.com>
2023-05-22 23:45:14 -0300

    archives for vercel deploy

---
+++ script_ladding.js
@@ -0,0 +1,30 @@
+const url$ = document.getElementById("btn_Consult");
+url$.addEventListener("click", () => {
+  window.location.href = "src/loginPage/index.html";
+});
+function toggleMenu() {
+  var menu = document.getElementById("menu-lateral");
+  menu.classList.toggle("mostrar");
+  if (mostrar === undefined) {
+    menu.classList.toggle("mostrar");
+  } else if (mostrar) {
+    menu.classList.add("mostrar");
+  } else {
+    menu.classList.remove("mostrar");
+  }
+}
+
+//carrossel:
+  $(document).ready(function(){
+    $('.carrossel').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      prevArrow: null,
+      cssEase: 'linear'
+    });
+  });
+
