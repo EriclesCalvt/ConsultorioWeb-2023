bb85ab6dd9dc39ee03b90d7f40943274618a1785
EriclesCalvt <ericlesprogrammer@gmail.com>
2023-06-18 12:17:00 -0300

    finished screen update data

--- src/Configuracao/alterarDados/index.js
+++ src/Configuracao/alterarDados/index.js
@@ -1,17 +1,74 @@
+// Variáveis globais para os elementos de input e o ID selecionado
+const inputNome = document.getElementById('input-nome');
+const inputRG = document.getElementById('RG');
+const inputCPF = document.getElementById('CPF');
+const inputTelefone = document.getElementById('telefone');
+const inputData = document.getElementById('Data');
+const inputCEP = document.getElementById('CEP');
+const inputCidade = document.getElementById('cidade');
+const inputBairro = document.getElementById('bairro');
+const inputRua = document.getElementById('rua');
+const inputNumeroCasa = document.getElementById('numero-casa');
+const selectSexo = document.getElementById('select-sexo');
+let selectedProductId = ''; // Declaração do ID selecionado
+
+// Função para enviar os dados atualizados para a API e atualizar no MongoDB
+function atualizarDados() {
+  // Obtenha os valores dos inputs atualizados
+  const nome = inputNome.value;
+  const rg = inputRG.value;
+  const cpf = inputCPF.value;
+  const telefone = inputTelefone.value;
+  const data = inputData.value;
+  const cep = inputCEP.value;
+  const cidade = inputCidade.value;
+  const bairro = inputBairro.value;
+  const rua = inputRua.value;
+  const numeroCasa = inputNumeroCasa.value;
+
+  // Construa o objeto com os dados atualizados
+  const dadosAtualizados = {
+    NomeProduct: nome,
+    Rg: rg,
+    Cpf: cpf,
+    Cep: cep,
+    Nascimento: data,
+    NumeroCasa: numeroCasa,
+    Rua: rua,
+    Bairro: bairro,
+    Cidade: cidade,
+    Telefone: telefone,
+  };
+
+  // Realize a requisição PUT para a API
+  fetch(`http://localhost:3000/Products/${selectedProductId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dadosAtualizados),
+  })
+    .then(response => response.json())
+    .then(data => {
+      alert("Sucesso!");
+      console.log('Dados atualizados com sucesso:', data);
+
+    })
+    .catch(error => {
+      alert("Ocorreu um erro!");
+      console.error('Erro ao atualizar os dados:', error);
+    });
+}
+
+// Adicione um evento ao formulário para chamar a função de atualização quando ocorrer um evento de envio
+const form = document.querySelector('.form-user');
+form.addEventListener('submit', event => {
+  event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+  atualizarDados();
+});
+
 // selecao de option
 document.addEventListener('DOMContentLoaded', () => {
-  const inputNome = document.getElementById('input-nome');
-  const inputRG = document.getElementById('RG');
-  const inputCPF = document.getElementById('CPF');
-  const inputTelefone = document.getElementById('telefone');
-  const inputData = document.getElementById('Data');
-  const inputCEP = document.getElementById('CEP');
-  const inputCidade = document.getElementById('cidade');
-  const inputBairro = document.getElementById('bairro');
-  const inputRua = document.getElementById('rua');
-  const inputNumeroCasa = document.getElementById('numero-casa');
-  const selectSexo = document.getElementById('select-sexo');
-
   fetch('http://localhost:3000/Products')
     .then(response => response.json())
     .then(data => {
@@ -29,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Preenchimento de dados quando selecionado o option
   selectSexo.addEventListener('change', () => {
-    const selectedProductId = selectSexo.value;
+    selectedProductId = selectSexo.value; // Atualiza o ID selecionado
 
     if (selectedProductId) {
       fetch(`http://localhost:3000/Products/${selectedProductId}`)
